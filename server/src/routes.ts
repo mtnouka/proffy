69a92f3bdf5ba7a608f43cb06abe870d8b8a46ea
mtnouka <mtheus.csouza@gmail.com>
2020-08-07 13:30:57 -0300

    Finalização do backend da aplicação.

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,13 +1,15 @@
 import express from 'express';
+import ClassesController from './controllers/ClassesController';
+import ConnectionsController from './controllers/ConnectionsController';
 
 const routes = express.Router();
+const classesControllers = new ClassesController();
+const connectionsController = new ConnectionsController();
 
-routes.post('/classes', (request, response) => {
-    const data = request.body;
+routes.get('/classes', classesControllers.index);
+routes.post('/classes', classesControllers.create);
 
-    console.log(data);
-
-    return response.send();
-});
+routes.get('/connections', connectionsController.index);
+routes.post('/connections', connectionsController.create);
 
 export default routes;
